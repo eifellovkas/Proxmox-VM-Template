fef4356d97f07cee24454cc6aef230caed538a94
eifellovkas <36507193+eifellovkas@users.noreply.github.com>
2025-06-28 21:55:36 +0200

    pokus gpt

--- build-image.sh
+++ build-image.sh
@@ -1,50 +1,61 @@
 #! /bin/sh
 
-# This script will download and modify the desired image to prep for template build.
-# Script is inspired by 2 separate authors work.
-# Austins Nerdy Things: https://austinsnerdythings.com/2021/08/30/how-to-create-a-proxmox-ubuntu-cloud-init-image/
-# What the Server: https://whattheserver.com/proxmox-cloud-init-os-template-creation/
-# requires libguestfs-tools to be installed.
-# This script is designed to be run inside the ProxMox VE host environment.
-# Modify the install_dir variable to reflect where you have placed the script and associated files.
+# Requires: libguestfs-tools, wget, qemu-utils, Proxmox VE environment
 
 . ./build-vars
 
 # Clean up any previous build
-rm ${install_dir}${image_name}
-rm ${install_dir}build-info
+rm -f ${install_dir}${image_name}
+rm -f ${install_dir}build-info
 rm -f ${install_dir}${image_name}_resized.qcow2
 
+# Download latest cloud-init image
+wget -O ${install_dir}${image_name} ${cloud_img_url}
 
-
-# Grab latest cloud-init image for your selected image
-wget ${cloud_img_url}
-
-# insert commands to populate the currently empty build-info file
+# Create build info
 touch ${install_dir}build-info
-echo "Base Image: "${image_name} > ${install_dir}build-info
-echo "Packages added at build time: "${package_list} >> ${install_dir}build-info
-echo "Build date: "$(date) >> ${install_dir}build-info
-echo "Build creator: "${creator} >> ${install_dir}build-info
+echo "Base Image: ${image_name}" > ${install_dir}build-info
+echo "Packages added at build time: ${package_list}" >> ${install_dir}build-info
+echo "Build date: $(date)" >> ${install_dir}build-info
+echo "Build creator: ${creator}" >> ${install_dir}build-info
 
-virt-customize --update -a ${image_name}
-virt-customize --install ${package_list} -a ${image_name}
-virt-customize --mkdir ${build_info_file_location} --copy-in ${install_dir}build-info:${build_info_file_location} -a ${image_name}
-# 🟢 NEW: Resize the disk safely with virt-resize
-# 1. Create an empty larger image
+# Customize image
+virt-customize --update -a ${install_dir}${image_name}
+virt-customize --install ${package_list} -a ${install_dir}${image_name}
+virt-customize --mkdir ${build_info_file_location} --copy-in ${install_dir}build-info:${build_info_file_location} -a ${install_dir}${image_name}
+
+# Identify correct root partition
+ROOT_PART=$(virt-filesystems -a ${install_dir}${image_name} --partition --all | grep '/dev/' | grep -vE 'boot|efi' | head -n1)
+
+# Resize disk image safely
 qemu-img create -f qcow2 ${install_dir}${image_name}_resized.qcow2 8G
+virt-resize --expand ${ROOT_PART} -a ${install_dir}${image_name} -o ${install_dir}${image_name}_resized.qcow2
 
-# 2. Expand the root partition to use all space
-virt-resize --expand /dev/sda2 -a ${install_dir}${image_name} -o ${install_dir}${image_name}_resized.qcow2
+# Build Proxmox VM Template with VirtIO
+qm destroy ${build_vm_id} 2>/dev/null
+qm create ${build_vm_id} \
+  --memory ${vm_mem} \
+  --cores ${vm_cores} \
+  --net0 virtio,bridge=vmbr0 \
+  --ipconfig0 ip=dhcp,ip6=auto \
+  --name ${template_name} \
+  --ostype l26 \
+  --boot c \
+  --bootdisk virtio0 \
+  --agent enabled=1
 
-
-qm destroy ${build_vm_id}
-qm create ${build_vm_id} --memory ${vm_mem} --cores ${vm_cores} --net0 virtio,bridge=vmbr0 --ipconfig0 ip=dhcp,ip6=auto --name ${template_name}
+# Import disk and attach as virtio
 qm importdisk ${build_vm_id} ${install_dir}${image_name}_resized.qcow2 ${storage_location}
-qm set ${build_vm_id} --scsihw ${scsihw} --virtio0 ${storage_location}:vm-${build_vm_id}-disk-0
-qm set ${build_vm_id} --ide0 ${storage_location}:cloudinit
-qm set ${build_vm_id} --nameserver ${nameserver} --ostype l26 --searchdomain ${searchdomain} --sshkeys ${keyfile} --ciuser ${cloud_init_user}
-qm set ${build_vm_id} --boot c --bootdisk virtio0
-#qm set ${build_vm_id} --serial0 socket --vga serial0
-qm set ${build_vm_id} --agent enabled=1
+qm set ${build_vm_id} --virtio0 ${storage_location}:vm-${build_vm_id}-disk-0
+
+# Cloud-init and other metadata
+qm set ${build_vm_id} \
+  --ide0 ${storage_location}:cloudinit \
+  --nameserver ${nameserver} \
+  --searchdomain ${searchdomain} \
+  --sshkeys ${keyfile} \
+  --ciuser ${cloud_init_user}
+
+# Finalize template
 qm template ${build_vm_id}
+
