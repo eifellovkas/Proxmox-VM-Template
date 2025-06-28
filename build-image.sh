dba0d27220b4be645d07b3bf038f67a49a5227f2
eifellovkas <36507193+eifellovkas@users.noreply.github.com>
2025-06-28 22:01:30 +0200

    pokus 159797

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 
 # Requires: libguestfs-tools, wget, qemu-utils, Proxmox VE environment
 
@@ -7,32 +7,32 @@
 # Clean up any previous build
 rm -f ${install_dir}${image_name}
 rm -f ${install_dir}build-info
-rm -f ${install_dir}${image_name}_resized.qcow2
 
-# Download latest cloud-init image
+# Download latest Debian cloud image
 wget -O ${install_dir}${image_name} ${cloud_img_url}
 
-# Create build info
+# Create build-info file
 touch ${install_dir}build-info
 echo "Base Image: ${image_name}" > ${install_dir}build-info
 echo "Packages added at build time: ${package_list}" >> ${install_dir}build-info
 echo "Build date: $(date)" >> ${install_dir}build-info
 echo "Build creator: ${creator}" >> ${install_dir}build-info
 
-# Customize image
-virt-customize --update -a ${install_dir}${image_name}
-virt-customize --install ${package_list} -a ${install_dir}${image_name}
-virt-customize --mkdir ${build_info_file_location} --copy-in ${install_dir}build-info:${build_info_file_location} -a ${install_dir}${image_name}
+# Customize image - add packages needed for autogrow
+virt-customize \
+  -a ${install_dir}${image_name} \
+  --update \
+  --install "${package_list} cloud-guest-utils" \
+  --mkdir ${build_info_file_location} \
+  --copy-in ${install_dir}build-info:${build_info_file_location}
 
-# Identify correct root partition
-ROOT_PART=$(virt-filesystems -a ${install_dir}${image_name} --partition --all | grep '/dev/' | grep -vE 'boot|efi' | head -n1)
+# Resize the raw disk image (filesystem will expand during boot)
+qemu-img resize ${install_dir}${image_name} +6G
 
-# Resize disk image safely
-qemu-img create -f qcow2 ${install_dir}${image_name}_resized.qcow2 8G
-virt-resize --expand ${ROOT_PART} -a ${install_dir}${image_name} -o ${install_dir}${image_name}_resized.qcow2
-
-# Build Proxmox VM Template with VirtIO
+# Remove any existing VM with same ID
 qm destroy ${build_vm_id} 2>/dev/null
+
+# Create the VM template
 qm create ${build_vm_id} \
   --memory ${vm_mem} \
   --cores ${vm_cores} \
@@ -44,11 +44,11 @@ qm create ${build_vm_id} \
   --bootdisk virtio0 \
   --agent enabled=1
 
-# Import disk and attach as virtio
-qm importdisk ${build_vm_id} ${install_dir}${image_name}_resized.qcow2 ${storage_location}
+# Import the customized disk
+qm importdisk ${build_vm_id} ${install_dir}${image_name} ${storage_location}
 qm set ${build_vm_id} --virtio0 ${storage_location}:vm-${build_vm_id}-disk-0
 
-# Cloud-init and other metadata
+# Attach cloud-init and metadata
 qm set ${build_vm_id} \
   --ide0 ${storage_location}:cloudinit \
   --nameserver ${nameserver} \
@@ -56,6 +56,5 @@ qm set ${build_vm_id} \
   --sshkeys ${keyfile} \
   --ciuser ${cloud_init_user}
 
-# Finalize template
+# Convert to template
 qm template ${build_vm_id}
-
